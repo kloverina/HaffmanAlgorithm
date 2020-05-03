0f46af32f4a15ce8a48a520b8988c0324009eedd
kloverina <45770461+kloverina@users.noreply.github.com>
2020-05-03 19:48:55 +0300

    Add files via upload

---
+++ Haffman_test.cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <exception>
+#include "../HaffmanAlgoritm/HaffmanAlgorithm.h"
+#include "../HaffmanAlgoritm/HaffmanAlgorithm.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Haffman_test
+{
+	TEST_CLASS(Haffman_test)
+	{
+	public:
+		
+		TEST_METHOD(Exeption_empty_string)
+		{
+			std::string text;
+			try
+			{
+				HaffmanTree Tree(text);
+			}
+			catch(std::out_of_range error)
+			{
+				Assert::AreEqual(error.what(), "String is empty!");
+			}
+		}
+		TEST_METHOD(GetCode_and_GetText_test)
+		{
+			std::string text("Это бессмысленная надпись");
+			HaffmanTree Tree(text);
+			std::string code = Tree.getCode(text);
+
+			Assert::AreEqual(text, Tree.getText(code, Tree));
+		}
+
+	};
+}
